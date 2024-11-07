dc9247928699e9e8f29594d0192f598af688ca48
Đorđe Blagojević <107189397+djordjeblagojevic@users.noreply.github.com>
2024-11-07 15:18:41 +0100

    EXT-9 / Add navbar (#3)
    * install shadcn components
    
    * add temp not-found page
    
    * create navbar component
    
    * create mobile menu for navbar
    
    * create a language selector component
    
    * create navbar config with all routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Typography from "../components/custom-ui/typography";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 sm:p-20">
+      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
         <Image
           className="dark:invert"
           src="/logo.svg"
@@ -13,10 +13,10 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left">
+        <ol className="list-inside list-decimal text-center text-sm sm:text-left">
           <li className="mb-2">
             Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+            <code className="rounded bg-black/[.05] px-1 py-0.5 font-semibold dark:bg-white/[.06]">
               src/app/page.tsx
             </code>
             .
---
+++ src/components/custom-ui/navbar/language-selector.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import * as React from "react";
+import { ChevronDown, Earth } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { cn } from "@/lib/utils";
+
+const LanguageSelector = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  return (
+    <DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          className="size-12 gap-0 rounded-full p-0 text-neutrals-50 hover:bg-inherit hover:text-neutrals-200 lg:size-8 lg:text-neutrals-800 hover:lg:bg-[#F8F7F2] hover:lg:text-inherit xl:size-12"
+        >
+          <Earth className="size-6 lg:size-4 xl:size-6" />
+          <ChevronDown
+            className={cn(
+              "size-6 transition-transform duration-200 lg:size-4 xl:size-6",
+              isOpen && "rotate-180",
+            )}
+          />
+          <span className="sr-only">Toggle language</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem>English</DropdownMenuItem>
+        <DropdownMenuItem>Srpski</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default LanguageSelector;
