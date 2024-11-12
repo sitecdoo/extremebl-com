b727bafe44d8161ff0547629d6b296679905516c
Andrej <andrejjurisic99@gmail.com>
2024-11-12 15:16:10 +0100

    create a card component

---
+++ src/components/custom-ui/card/card.tsx
@@ -0,0 +1,66 @@
+import Image from "next/image";
+import Typography from "../typography";
+import { Clock } from "lucide-react";
+import Pill from "./pill";
+import Link from "next/link";
+
+type CardProps = {
+  image: string;
+  time: string;
+  title: string;
+  description: string;
+  tags: string[];
+};
+
+const Card = ({ image, time, title, description, tags }: CardProps) => {
+  return (
+    <div className="flex min-h-[28.75rem] min-w-60 max-w-[22.375rem] flex-col gap-3 rounded-2xl bg-neutrals-50 p-3 pb-4 hover:bg-neutrals-100 lg:min-h-[42.5rem] lg:max-w-[33.125rem] lg:gap-4 lg:rounded-28 lg:p-4 lg:pb-6">
+      <Link href="/blog">
+        <Image
+          className="h-[11.375rem] rounded-2xl object-cover lg:h-[18.75rem] lg:rounded-18"
+          src={image}
+          width={498}
+          height={300}
+          alt={`${title}-image`}
+          priority
+        />
+      </Link>
+      <div className="flex w-full flex-grow flex-col gap-3 lg:gap-4">
+        <div className="flex items-center gap-x-1 text-neutrals-500">
+          <Clock className="size-5" />
+          <Typography variant="caption">{time}</Typography>
+        </div>
+        <div className="mb-4 flex flex-grow flex-col gap-2 lg:mb-8 lg:gap-3">
+          <Link href={`/blog/`}>
+            {/* Title for Mobile */}
+            <Typography
+              variant="h5"
+              tag="h5"
+              fontWeight="bold"
+              className="lg:hidden"
+            >
+              {title}
+            </Typography>
+            {/* Title for Desktop */}
+            <Typography
+              variant="h4"
+              tag="h4"
+              fontWeight="bold"
+              className="hidden lg:block"
+            >
+              {title}
+            </Typography>
+          </Link>
+          <Typography variant="body-sm">{description}</Typography>
+        </div>
+        <div className="mt-auto flex flex-wrap items-center gap-2">
+          {tags.map((name, index) => (
+            <Pill key={index} name={name} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
