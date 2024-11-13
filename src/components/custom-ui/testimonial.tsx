0ece87b688712c606be89663ddc9e109e7df238b
Andrej <andrejjurisic99@gmail.com>
2024-11-13 13:38:32 +0100

    update component to work on all screens

--- src/components/custom-ui/testimonial.tsx
+++ src/components/custom-ui/testimonial.tsx
@@ -1,26 +1,33 @@
 import Typography from "./typography";
 import Image from "next/image";
 
-const Testimonial = () => {
+type TesimonialProps = {
+  image: string;
+  name: string;
+  description: string;
+};
+
+const Testimonial = ({ image, name, description }: TesimonialProps) => {
   return (
-    <div className="flex w-full flex-col items-center justify-center gap-6 rounded-2xl bg-neutrals-100 px-9 py-10">
-      <div className="flex items-center gap-4">
-        {/* Added style to disable console warning */}
-        <Image
-          width={36}
-          height={36}
-          src="/marija-peric.jpg"
-          alt="marija-peric"
-          style={{ width: 36, height: 36 }}
-          className="rounded-full object-cover"
-        />
-        <Typography fontWeight="bold">Marija Peric</Typography>
+    <div className="sm:px-16 lg:px-20 xl:px-36">
+      <div className="flex w-full flex-col items-center justify-center gap-6 rounded-2xl bg-neutrals-100 px-9 py-10 lg:gap-10 lg:px-20 lg:py-20 xl:px-40 2xl:px-64">
+        <div className="flex items-center gap-4">
+          <Image
+            width={480}
+            height={321}
+            src={image}
+            alt={`${name}-image`}
+            className="size-9 rounded-full object-cover"
+            priority
+          />
+          <Typography fontWeight="bold" uppercase>
+            {name}
+          </Typography>
+        </div>
+        <Typography variant="subtitle" className="text-center">
+          {description}
+        </Typography>
       </div>
-      <Typography variant="subtitle" className="text-center">
-        “Ja sam oduševljena. Instruktori su za čistu desetku, nevjerovatni
-        motivatori... Proslavili smo 14 rođendan i slavljenica kaže da joj je to
-        bio najbolji rođendan!”
-      </Typography>
     </div>
   );
 };
