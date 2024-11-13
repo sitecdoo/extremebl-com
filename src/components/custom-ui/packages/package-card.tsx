c4b31e6f7be2b98bb270739cf75993c379258aed
Andrej <andrejjurisic99@gmail.com>
2024-11-13 14:50:44 +0100

    update package card component for all sizes

--- src/components/custom-ui/packages/package-card.tsx
+++ src/components/custom-ui/packages/package-card.tsx
@@ -9,8 +9,8 @@ type PackageCardProps = {
 
 const PackageCard = ({ name, price, reward }: PackageCardProps) => {
   return (
-    <div className="flex w-full min-w-60 max-w-80 flex-col rounded-20 bg-neutrals-100 px-12 py-8 shadow-sm">
-      <div className="flex flex-col gap-6">
+    <div className="flex w-full min-w-60 max-w-80 flex-col gap-y-8 rounded-20 bg-neutrals-100 px-12 py-8 shadow-sm lg:max-w-[22.5rem] lg:gap-y-12 lg:px-14 lg:py-12">
+      <div className="flex flex-col gap-y-6 lg:gap-y-9">
         <div className="flex w-fit flex-col items-center gap-3 self-center">
           <Typography variant="body-sm" uppercase>
             {name}
@@ -29,10 +29,10 @@ const PackageCard = ({ name, price, reward }: PackageCardProps) => {
             </div>
           ))}
         </div>
-        <button className="mt-2 w-fit self-center rounded-full bg-blue-500 px-5 py-4 text-neutrals-50">
-          <Typography fontWeight="bold">Rezerviši termin</Typography>
-        </button>
       </div>
+      <button className="w-fit self-center rounded-full bg-blue-500 px-5 py-4 text-neutrals-50 lg:px-6">
+        <Typography fontWeight="bold">Rezerviši termin</Typography>
+      </button>
     </div>
   );
 };
