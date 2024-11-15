426a49584a25e6f7c847ad8f805a8c8442ea2f1a
Andrej <andrejjurisic99@gmail.com>
2024-11-15 11:57:45 +0100

    update wrapper component

--- src/components/custom-ui/blog/recent-blog-wrapper/recent-blog-wrapper.tsx
+++ src/components/custom-ui/blog/recent-blog-wrapper/recent-blog-wrapper.tsx
@@ -1,14 +1,24 @@
+import { Button } from "@/components/ui/button";
 import Typography from "../../typography";
 import SmallCard from "./small-card";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
 
 const RecentBlogWrapper = () => {
   return (
-    <div className="flex flex-col gap-4">
-      <button>Blog</button>
-      <Typography variant="h2" tag="h2" fontWeight="bold">
-        Najnovije objave
-      </Typography>
-      <div className="grid grid-cols-1 justify-items-center gap-3 rounded-xl bg-neutrals-200 p-3 md:grid-cols-2 md:gap-5 md:p-10 lg:grid-cols-3 lg:rounded-40 lg:px-5 xl:gap-9 xl:px-10">
+    <div className="flex flex-col items-center gap-2 lg:gap-4">
+      <div className="flex flex-col items-center gap-4 lg:gap-5">
+        <Button
+          variant="black"
+          className="pointer-events-none py-2 lg:px-5 lg:py-2"
+        >
+          <Typography>Blog</Typography>
+        </Button>
+        <Typography variant="h2" tag="h2" fontWeight="bold">
+          Najnovije objave
+        </Typography>
+      </div>
+      <div className="mt-2 grid grid-cols-1 justify-items-center gap-3 rounded-xl bg-neutrals-200 p-3 md:grid-cols-2 md:gap-5 md:p-10 lg:mt-8 lg:grid-cols-3 lg:rounded-40 lg:px-5 xl:gap-9 xl:px-10">
         {Array.from({ length: 3 }).map((_, index) => (
           <SmallCard
             key={index}
@@ -20,6 +30,12 @@ const RecentBlogWrapper = () => {
           />
         ))}
       </div>
+      <Link href="/blog" className="self-end">
+        <Button variant="ghost" className="flex items-center gap-x-2">
+          <Typography fontWeight="bold">Vidi sve postove</Typography>
+          <ArrowRight className="size-5 text-neutrals-900" />
+        </Button>
+      </Link>
     </div>
   );
 };
