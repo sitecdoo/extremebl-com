18da3875953d7d30a4e6a00074e78e74091c7842
Andrej <andrejjurisic99@gmail.com>
2024-11-15 16:29:51 +0100

    add component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { leagueSpartan, openSans } from "@/styles/fonts";
 import "./globals.css";
 import type { Metadata } from "next";
 import Navbar from "@/components/custom-ui/navbar/navbar";
+import Questions from "@/components/custom-ui/questions";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,8 +20,9 @@ export default function RootLayout({
         className={`${leagueSpartan.variable} ${openSans.variable} font-league-spartan antialiased`}
       >
         <Navbar />
-        <div className="mx-auto flex w-full max-w-[108rem] flex-col items-center gap-16 px-4 lg:px-12">
-          {children}
+        <div className="mx-auto flex w-full max-w-[108rem] flex-col items-center gap-16 px-4 py-10 lg:px-12">
+          <Questions />
+          {/* {children} */}
         </div>
       </body>
     </html>
