1b8563bb2f51de3fad58067ad54d85aa8a6088fc
Andrej <andrejjurisic99@gmail.com>
2024-11-20 15:06:17 +0100

    update testemonial component

--- src/components/custom-ui/testimonial/testimonial-section.tsx
+++ src/components/custom-ui/testimonial/testimonial-section.tsx
@@ -2,13 +2,13 @@ import { Button } from "@/components/ui/button";
 import Testimonial from "./testimonial";
 import Typography from "../typography";
 
-const TestimonialSection = () => {
+const TestimonialSection = ({ description }: { description: string }) => {
   return (
     <div className="flex w-full flex-col items-center gap-6 sm:px-10 md:px-16 lg:gap-12 lg:px-20 xl:px-36">
       <Testimonial
         image="/marija-peric.jpg"
         name="Marija Peric"
-        description="Najviše mi se svidjelo kako su aktivnosti bile prilagođene našim sposobnostima, omogućujući svakom članu tima da se osjeća uključenim i podržanim. Ovo iskustvo je definitivno poboljšalo našu timsku dinamiku i preporučujemo ga svakome tko želi ojačati svoj tim kroz zabavu i avanturu!"
+        description={description}
       />
       <Button className="w-fit" variant="blue">
         <Typography fontWeight="bold">Kontaktiraj nas</Typography>
@@ -16,5 +16,4 @@ const TestimonialSection = () => {
     </div>
   );
 };
-
 export default TestimonialSection;
