85bb84ddc3f816337482456a740cd28f05e6bc13
AndrejJurisic <124591321+AndrejJurisic@users.noreply.github.com>
2024-11-22 11:51:04 +0100

    EXT-23 / Create Questions Section (#15)
    * install shadcn accordion component
    
    * create questions component
    
    * add component to layout
    
    * add padding for lg screens
    
    
    ---------
    
    Co-authored-by: Djordje Blagojevic <dj.blagojevic@gmail.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -121,6 +121,28 @@ const config: Config = {
         40: "2.5rem",
         60: "3.75rem",
       },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
