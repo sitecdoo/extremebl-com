57a8a94f2f93cfd773b73bc0e884e38c2004fcf2
Andrej <andrejjurisic99@gmail.com>
2024-11-25 15:53:09 +0100

    update location on google maps

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -59,7 +59,7 @@ const ContactPage = () => {
           </Typography>
           <div className="h-[18rem] w-full lg:h-[43.75rem]">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2831.3385769172913!2d17.191234776191386!3d44.77214177107333!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475e03128fa5d899%3A0x3fb89f4b18a763b0!2sBanja%20Luka!5e0!3m2!1sen!2sba!4v1709856547372!5m2!1sen!2sba"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1416.1124305166602!2d17.21070856139592!3d44.77622092187391!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475e030cc563ea03%3A0x7cfedc831b0390d5!2sPenja%C4%8Dki%20klub%20Extreme!5e0!3m2!1sbs!2sba!4v1732545511105!5m2!1sbs!2sba"
               style={{ border: 0 }}
               allowFullScreen
               loading="lazy"
