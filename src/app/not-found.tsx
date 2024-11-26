2b9564afbe9df9fb9da9a73c7429f235b4d51d6e
Djordje Blagojevic <dj.blagojevic@gmail.com>
2024-11-26 11:39:24 +0100

    add a not found page in app directory

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,24 +1,36 @@
 import Link from "next/link";
 import { Home, AlertCircle } from "lucide-react";
+import "./globals.css";
+import Navbar from "@/components/custom-ui/navbar/navbar";
+import Footer from "@/components/custom-ui/footer";
+import { leagueSpartan, openSans } from "@/styles/fonts";
 
 const NotFound = () => {
   return (
-    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-gray-100">
-      <div className="mb-4 text-red-500">
-        <AlertCircle size={48} />
-      </div>
-      <h2 className="mb-2 text-2xl font-bold">Not Found</h2>
-      <p className="mb-4 text-gray-600">
-        Could not find the requested resource
-      </p>
-      <Link
-        href="/"
-        className="flex items-center text-blue-500 hover:underline"
+    <html lang="en">
+      <body
+        className={`${leagueSpartan.variable} ${openSans.variable} bg-neutrals-50 font-league-spartan antialiased`}
       >
-        <Home className="mr-2" size={20} />
-        Return Home
-      </Link>
-    </div>
+        <Navbar />
+        <div className="flex min-h-screen w-full flex-col items-center justify-center bg-neutrals-50">
+          <div className="mb-4 text-red-500">
+            <AlertCircle size={48} />
+          </div>
+          <h2 className="mb-2 text-2xl font-bold">Not Found</h2>
+          <p className="mb-4 text-gray-600">
+            Could not find the requested resource
+          </p>
+          <Link
+            href="/"
+            className="flex items-center text-blue-500 hover:underline"
+          >
+            <Home className="mr-2" size={20} />
+            Return Home
+          </Link>
+        </div>
+        <Footer />
+      </body>
+    </html>
   );
 };
 
