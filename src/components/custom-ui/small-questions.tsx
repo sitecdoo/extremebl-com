4b529ac67a3e0083f93327766592cbfc22d30ea0
Andrej <andrejjurisic99@gmail.com>
2024-11-27 12:32:43 +0100

    add animation for text

--- src/components/custom-ui/small-questions.tsx
+++ src/components/custom-ui/small-questions.tsx
@@ -25,6 +25,20 @@ const questions = [
 
 const SmallQuestions = () => {
   const [openIndex, setOpenIndex] = useState<number>(0);
+  const [isVisible, setIsVisible] = useState<boolean>(true);
+
+  const handleClick = (index: number) => {
+    if (openIndex === index) {
+      return;
+    }
+
+    setIsVisible(false);
+    setTimeout(() => {
+      setOpenIndex(index);
+      setIsVisible(true);
+    }, 300);
+  };
+
   return (
     <div className="flex flex-col gap-8 self-start lg:flex-row lg:items-center lg:justify-between lg:gap-10 lg:self-center xl:gap-20 2xl:gap-40">
       <div className="space-y-4 lg:space-y-8">
@@ -33,20 +47,18 @@ const SmallQuestions = () => {
             <div
               key={index}
               className={cn(
-                "flex w-fit cursor-pointer items-center gap-x-4 xl:max-w-[453px]",
+                "flex w-fit cursor-pointer items-center gap-x-4 xl:max-w-[28.5rem]",
                 openIndex === index && "cursor-default",
               )}
-              onClick={() => setOpenIndex(index)}
+              onClick={() => handleClick(index)}
             >
-              {openIndex === index ? (
-                <div className="size-6 lg:size-9">
-                  <Minus className="size-6 rounded-full border-2 border-neutrals-800 p-1 lg:size-9" />
-                </div>
-              ) : (
-                <div className="size-6 lg:size-9">
-                  <Plus className="size-6 rounded-full border-2 border-neutrals-300 p-1 text-neutrals-300 lg:size-9" />
-                </div>
-              )}
+              <div className="size-6 sm:size-7 lg:size-9">
+                {openIndex === index ? (
+                  <Minus className="size-6 rounded-full border-2 border-neutrals-800 p-1 sm:size-7 lg:size-9" />
+                ) : (
+                  <Plus className="size-6 rounded-full border-2 border-neutrals-300 p-1 text-neutrals-300 sm:size-7 lg:size-9" />
+                )}
+              </div>
               <Typography
                 variant="h4"
                 tag="h4"
@@ -62,7 +74,12 @@ const SmallQuestions = () => {
           );
         })}
       </div>
-      <Typography className="min-h-72 transition-all duration-300 ease-in-out sm:min-h-36 md:min-h-32 lg:min-h-64 lg:max-w-xl">
+      <Typography
+        className={cn(
+          "min-h-72 transition-all duration-300 ease-in-out sm:min-h-36 md:min-h-32 lg:min-h-64 lg:max-w-xl",
+          isVisible ? "opacity-100" : "opacity-0",
+        )}
+      >
         {questions[openIndex].answer}
       </Typography>
     </div>
