621a8991f946bbae762a55001ab8bc3ed4b1a76a
Andrej <andrejjurisic99@gmail.com>
2024-11-28 12:03:51 +0100

    ...

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
   const body = await req.json();
-  console.log("Received body:", body);
+
   try {
     const { data, error } = await resend.emails.send({
       from: "Acme <onboarding@resend.dev>",
