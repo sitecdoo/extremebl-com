b53e912d7d330da3951cd0ef756914177816e870
Andrej <andrejjurisic99@gmail.com>
2024-12-03 16:12:48 +0100

    upodate next image to work with instagram

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.cdninstagram.com",
+      },
+    ],
+  },
 };
 
 export default withPayload(nextConfig);
