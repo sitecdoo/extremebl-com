ec5282630e0eca4dcf0adac7bdddab71c7b6dd63
Djordje Blagojevic <dj.blagojevic@gmail.com>
2024-12-04 16:02:37 +0100

    ...

--- src/components/custom-ui/instagram-posts.tsx
+++ src/components/custom-ui/instagram-posts.tsx
@@ -20,20 +20,20 @@ const InstagramPosts = ({ posts }: InstagramPostsProps) => {
         posts.map((post) => (
           <div
             key={post.id}
-            className="max-w-44 sm:max-w-72 lg:max-h-96 lg:max-w-96"
+            className="h-44 max-w-44 sm:h-72 sm:max-w-72 lg:h-full lg:max-h-96 lg:max-w-96"
           >
             {post.media_type === "IMAGE" ||
             post.media_type === "CAROUSEL_ALBUM" ? (
               <Image
                 src={post.media_url}
                 alt={post.caption || "Instagram Post"}
-                className="rounded-xl lg:rounded-28"
+                className="h-full rounded-xl lg:rounded-28"
                 width={384}
                 height={384}
                 priority
               />
             ) : (
-              <video controls className="rounded-xl lg:rounded-28">
+              <video controls className="h-full rounded-xl lg:rounded-28">
                 <source src={post.media_url} type="video/mp4" />
               </video>
             )}
