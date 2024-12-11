dc0c632f4d738d17a94fd95508c8dce18187c96a
Andrej <andrejjurisic99@gmail.com>
2024-12-11 11:48:43 +0100

    add blobs to children page

---
+++ src/components/custom-ui/blobs/children/index.ts
@@ -0,0 +1,5 @@
+import ChildrenBannerBlobs from "./children-banner-blobs";
+import ChildrenCarouselBlobs from "./children-carousel-blobs";
+import ChildrenBenefitsBlobs from "./children-benefits-blobs";
+
+export { ChildrenBannerBlobs, ChildrenCarouselBlobs, ChildrenBenefitsBlobs };
