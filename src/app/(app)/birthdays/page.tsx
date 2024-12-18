e0627b8d094bd79e8a23661d427f80fbc701e535
Andrej <andrejjurisic99@gmail.com>
2024-12-18 14:46:09 +0100

    update birthdays data structure

--- src/app/(app)/birthdays/page.tsx
+++ src/app/(app)/birthdays/page.tsx
@@ -3,13 +3,13 @@ import InfoSection from "@/components/custom-ui/info-section";
 import Masonry from "@/components/custom-ui/masonry";
 import { PackagesSection } from "@/components/custom-ui/packages";
 import React from "react";
-import { birthdayImages } from "../config/masonry-config";
 import { TestimonialSection } from "@/components/custom-ui/testimonial";
 import Header from "@/components/custom-ui/header";
 import {
   BirthdaysBannerBlobs,
   BirthdaysTestemonialBlobs,
 } from "@/components/custom-ui/blobs/birthdays";
+import { masonryImages, infoSectionData } from "@/content/birthdays";
 
 const Birthdays = () => {
   return (
@@ -20,24 +20,17 @@ const Birthdays = () => {
         <Header text="Proslavite rođendan vašeg deteta na jedinstven i uzbudljiv način! Naša penjačka sala nudi savršeno okruženje za nezaboravnu rođendansku proslavu." />
       </div>
       <div className="flex w-full flex-col gap-y-24 lg:gap-y-32">
-        <InfoSection
-          image="/birthdays/info-section-1.jpg"
-          title="Profesionalni instruktori"
-          description="Sportsko penjanje je oblik penjanja po stijenama ili umjetnim stijenama u kontroliranom okruženju, gdje se penjači oslanjaju na fizičku snagu, tehniku i izdržljivost kako bi savladali različite penjačke rute. Ova aktivnost uključuje korištenje sigurnosn"
-        />
-        <InfoSection
-          image="/birthdays/info-section-2.jpg"
-          title="Sigurna oprema"
-          description="Sportsko penjanje je oblik penjanja po stijenama ili umjetnim stijenama u kontroliranom okruženju, gdje se penjači oslanjaju na fizičku snagu, tehniku i izdržljivost kako bi savladali različite penjačke rute. Ova aktivnost uključuje korištenje sigurnosn"
-        />
-        <InfoSection
-          image="/birthdays/info-section-3.jpg"
-          title="Zabava za sve uzraste"
-          description="Sportsko penjanje je oblik penjanja po stijenama ili umjetnim stijenama u kontroliranom okruženju, gdje se penjači oslanjaju na fizičku snagu, tehniku i izdržljivost kako bi savladali različite penjačke rute. Ova aktivnost uključuje korištenje sigurnosn"
-        />
+        {infoSectionData.map((info, index) => (
+          <InfoSection
+            key={index}
+            image={info.image}
+            title={info.title}
+            description={info.description}
+          />
+        ))}
       </div>
       <PackagesSection />
-      <Masonry images={birthdayImages} />
+      <Masonry images={masonryImages} />
       <TestimonialSection description="“Ja sam oduševljena. Instruktori su za čistu desetku, nevjerovatni motivatori... Proslavili smo 14 rođendan i slavljenica kaže da joj je to bio najbolji rođendan!”">
         <BirthdaysTestemonialBlobs />
       </TestimonialSection>
