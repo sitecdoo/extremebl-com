c24a77519e8273a464e7fdab4af85e4a258f7dee
Andrej <andrejjurisic99@gmail.com>
2024-12-18 14:45:45 +0100

    update adult data structure

---
+++ src/content/adults.ts
@@ -0,0 +1,31 @@
+export const pricingData = {
+  schedule: {
+    title: "Trening za odrasle",
+    date: [
+      { day: "Utorak", time: "19:00 - 22:00h" },
+      { day: "Srijeda", time: "19:00 - 22:00h" },
+      { day: "Četvrtak", time: "19:00 - 22:00h" },
+      { day: "Nedjelja", time: "19:00 - 22:00h" },
+    ],
+  },
+  prices: [
+    { text: "Dnevna karta / Probni trening", price: "10 KM" },
+    { text: "Mjesečna članarina", price: "45 KM" },
+    { text: "10 ulazaka", price: "70 KM" },
+    { text: "Polugodišnja članarina", price: "240 KM" },
+    { text: "Godišnja članarina", price: "420 KM" },
+  ],
+};
+
+export const carouselImages = [
+  { src: "/adults/adults-carousel-1.jpg", alt: "carousel image" },
+  { src: "/adults/adults-carousel-2.jpg", alt: "carousel image" },
+  { src: "/adults/adults-carousel-3.jpg", alt: "carousel image" },
+  { src: "/adults/adults-carousel-4.jpg", alt: "carousel image" },
+  { src: "/adults/adults-carousel-5.jpg", alt: "carousel image" },
+  { src: "/adults/adults-carousel-6.jpg", alt: "carousel image" },
+  { src: "/adults/adults-carousel-7.jpg", alt: "carousel image" },
+  { src: "/adults/adults-carousel-8.jpg", alt: "carousel image" },
+  { src: "/adults/adults-carousel-9.jpg", alt: "carousel image" },
+  { src: "/adults/adults-carousel-10.jpg", alt: "carousel image" },
+];
