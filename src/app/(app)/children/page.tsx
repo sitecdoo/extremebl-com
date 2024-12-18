4213aaf85fcafd17f77dc060ad251df13ab87482
Andrej <andrejjurisic99@gmail.com>
2024-12-18 14:46:31 +0100

    update children data structure

--- src/app/(app)/children/page.tsx
+++ src/app/(app)/children/page.tsx
@@ -3,24 +3,23 @@ import { BenefitsCard, BenefitsSection } from "@/components/custom-ui/benefits";
 import { benefitsForChildren } from "@/components/custom-ui/benefits/benefits-data";
 import ImageCarousel from "@/components/custom-ui/image-carousel";
 import InfoSection from "@/components/custom-ui/info-section";
-import {
-  ListItem,
-  PriceList,
-  PricingData,
-} from "@/components/custom-ui/pricing";
+import { ListItem, PriceList } from "@/components/custom-ui/pricing";
 import Typography from "@/components/custom-ui/typography";
 import { Button } from "@/components/ui/button";
 import React from "react";
-import { carouselImages } from "./config";
 import Header from "@/components/custom-ui/header";
 import {
   ChildrenBannerBlobs,
   ChildrenBenefitsBlobs,
   ChildrenCarouselBlobs,
 } from "@/components/custom-ui/blobs/children";
+import {
+  carouselImages,
+  infoSectionData,
+  pricingData,
+} from "@/content/children";
 
 const Children = () => {
-  const data = PricingData;
   return (
     <div className="relative flex w-full flex-col items-center gap-24 pb-24 lg:gap-64 lg:pb-64">
       <ChildrenBannerBlobs />
@@ -29,18 +28,19 @@ const Children = () => {
         <Header text="Penjanje je nevjerovatna aktivnost za djecu koja poboljšava snagu, koordinaciju i vještine rješavanja problema. Povećava samopouzdanje, pruža osjećaj uspjeha i podstiče timski rad." />
       </div>
       <div className="flex w-full flex-col gap-y-24 lg:gap-y-32">
-        <InfoSection
-          image="/children/info-section-1.jpg"
-          title="Djeca mlađeg uzrasta"
-          description="Sportsko penjanje je oblik penjanja po stijenama ili umjetnim stijenama u kontroliranom okruženju, gdje se penjači oslanjaju na fizičku snagu, tehniku i izdržljivost kako bi savladali različite penjačke rute. Ova aktivnost uključuje korištenje sigurnosn"
-        />
-        <InfoSection
-          image="/children/info-section-2.jpg"
-          title="Djeca starijeg uzrasta"
-          description="Sportsko penjanje je oblik penjanja po stijenama ili umjetnim stijenama u kontroliranom okruženju, gdje se penjači oslanjaju na fizičku snagu, tehniku i izdržljivost kako bi savladali različite penjačke rute. Ova aktivnost uključuje korištenje sigurnosn"
-        />
+        {infoSectionData.map((info, index) => (
+          <InfoSection
+            key={index}
+            image={info.image}
+            title={info.title}
+            description={info.description}
+          >
+            <Button variant="yellow" className="w-fit">
+              <Typography fontWeight="bold">Prijavi se</Typography>
+            </Button>
+          </InfoSection>
+        ))}
       </div>
-
       <BenefitsSection title="Benefiti penjanja za djecu">
         {benefitsForChildren.map((data, index) => (
           <BenefitsCard
@@ -52,15 +52,10 @@ const Children = () => {
         ))}
         <ChildrenBenefitsBlobs />
       </BenefitsSection>
-      <PriceList pricingData={data.childrenPrices}>
-        <ListItem
-          title={data.youngerChildren.title}
-          data={data.youngerChildren.date}
-        />
-        <ListItem
-          title={data.olderChildren.title}
-          data={data.olderChildren.date}
-        />
+      <PriceList pricingData={pricingData.prices}>
+        {pricingData.schedules.map((schedule, index) => (
+          <ListItem key={index} title={schedule.title} data={schedule.date} />
+        ))}
       </PriceList>
       <ImageCarousel images={carouselImages}>
         <ChildrenCarouselBlobs />
---
+++ src/content/children.ts
@@ -0,0 +1,54 @@
+export const pricingData = {
+  schedules: [
+    {
+      title: "Trening za djecu mladjeg uzrasta",
+      date: [
+        { day: "Ponedjeljak", time: "18:30 - 19:30h" },
+        { day: "Petak", time: "18:30 - 19:30h" },
+      ],
+    },
+    {
+      title: "Trening za djecu starijeg uzrasta",
+      date: [
+        { day: "Ponedjeljak", time: "19:00 - 20:00h" },
+        { day: "Petak", time: "19:00 - 20:00h" },
+      ],
+    },
+  ],
+  prices: [
+    { text: "Probni trening", price: "10 KM" },
+    { text: "Mjesečna članarina", price: "50 KM" },
+    {
+      text: "Mjesečna članarina (dvoje djece iz iste porodice)",
+      price: "250 KM",
+    },
+  ],
+};
+
+export const infoSectionData = [
+  {
+    image: "/children/info-section-1.jpg",
+    title: "Djeca mlađeg uzrasta",
+    description:
+      "Sportsko penjanje je oblik penjanja po stijenama ili umjetnim stijenama u kontroliranom okruženju, gdje se penjači oslanjaju na fizičku snagu, tehniku i izdržljivost kako bi savladali različite penjačke rute. Ova aktivnost uključuje korištenje sigurnosn",
+  },
+  {
+    image: "/children/info-section-2.jpg",
+    title: "Djeca starijeg uzrasta",
+    description:
+      "Sportsko penjanje je oblik penjanja po stijenama ili umjetnim stijenama u kontroliranom okruženju, gdje se penjači oslanjaju na fizičku snagu, tehniku i izdržljivost kako bi savladali različite penjačke rute. Ova aktivnost uključuje korištenje sigurnosn",
+  },
+];
+
+export const carouselImages = [
+  { src: "/children/carousel-1.jpg", alt: "Children stretching at the gym" },
+  { src: "/children/carousel-2.jpg", alt: "Child climbing the gym wall" },
+  { src: "/children/carousel-3.jpg", alt: "Child climbing the gym wall" },
+  { src: "/children/carousel-4.jpg", alt: "Child climbing the gym wall" },
+  { src: "/children/carousel-5.jpg", alt: "Children group photo at the gym" },
+  { src: "/children/carousel-6.jpg", alt: "Children climbing the gym wall" },
+  { src: "/children/carousel-7.jpg", alt: "Child climbing the gym wall" },
+  { src: "/children/carousel-8.jpg", alt: "Children stretching at the gym" },
+  { src: "/children/carousel-9.jpg", alt: "Children stretching at the gym" },
+  { src: "/children/carousel-10.jpg", alt: "Child climbing the wall" },
+];
