6b557261a3940c96cdab1dfd53cb966c5c3476c0
Andrej <andrejjurisic99@gmail.com>
2024-12-18 14:45:23 +0100

    update pricing data structure

--- src/components/custom-ui/pricing/index.ts
+++ src/components/custom-ui/pricing/index.ts
@@ -1,5 +1,4 @@
 import PriceList from "./price-list";
 import ListItem from "./list-item";
-import { PricingData } from "./pricing-data";
 
-export { PriceList, ListItem, PricingData };
+export { PriceList, ListItem };
