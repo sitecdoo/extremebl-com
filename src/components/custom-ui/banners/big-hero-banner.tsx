6ed812b1ff5c7e67e33e39c52c24af9f2dc7cb8b
Andrej <andrejjurisic99@gmail.com>
2024-12-23 14:23:16 +0100

    add arrow icon to banner and change height for mobile

--- src/components/custom-ui/banners/big-hero-banner.tsx
+++ src/components/custom-ui/banners/big-hero-banner.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Typography from "../typography";
 import { Button } from "@/components/ui/button";
+import { MoveDown } from "lucide-react";
 
 const BigHeroBanner = () => {
   return (
@@ -10,11 +11,11 @@ const BigHeroBanner = () => {
         alt="hero-banner"
         width={5379}
         height={4016}
-        className="h-[47rem] min-w-64 rounded-2xl object-cover object-[70%] sm:w-full lg:h-[62.5rem] lg:rounded-40 lg:object-bottom"
+        className="h-[40rem] min-w-64 rounded-2xl object-cover object-[70%] sm:w-full lg:h-[62.5rem] lg:rounded-40 lg:object-bottom"
         priority
       />
       <div className="absolute inset-0 rounded-2xl bg-gradient-to-b from-dark-500/0 from-40% to-dark-500 lg:rounded-40 lg:bg-gradient-to-l lg:to-dark-500/50" />
-      <div className="absolute left-0 top-1/2 px-4 text-neutrals-50 sm:left-10 sm:max-w-xl sm:px-0 lg:left-14 lg:top-1/3 lg:max-w-2xl 2xl:left-32">
+      <div className="absolute bottom-10 left-0 px-4 text-neutrals-50 sm:bottom-20 sm:left-10 sm:max-w-xl sm:px-0 lg:left-14 lg:top-1/3 lg:max-w-2xl 2xl:left-32">
         {/*Banner title for desktop */}
         <Typography
           variant="display"
@@ -49,6 +50,14 @@ const BigHeroBanner = () => {
         </div>
       </div>
       <div />
+      <Image
+        src="/icons/arrow-down-icon.svg"
+        alt="arrow down"
+        width={25}
+        height={57}
+        className="absolute bottom-16 right-16 hidden lg:block"
+      />
+      <MoveDown className="absolute bottom-10 right-4 size-6 text-neutrals-50 sm:right-8 sm:size-8 lg:hidden" />
     </section>
   );
 };
