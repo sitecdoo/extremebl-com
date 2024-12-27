6d9704a96b3cbd06b57c8f98c6e9901780a57628
Andrej <andrejjurisic99@gmail.com>
2024-12-27 13:11:08 +0100

    update info section

--- src/components/custom-ui/info-section.tsx
+++ src/components/custom-ui/info-section.tsx
@@ -1,14 +1,13 @@
 import Image from "next/image";
 import Typography from "./typography";
 import { PropsWithChildren } from "react";
-import { cn } from "@/lib/utils";
 
 type InfoSectionProps = PropsWithChildren<{
   image: string;
   title: string;
   description: string;
-  position?: "object-top" | "object-bottom" | "object-right" | "object-left";
-  smPosition?: boolean;
+  xPosition?: number;
+  yPosition?: number;
 }>;
 
 const InfoSection = ({
@@ -16,8 +15,8 @@ const InfoSection = ({
   title,
   description,
   children,
-  position,
-  smPosition,
+  xPosition = 50,
+  yPosition = 50,
 }: InfoSectionProps) => {
   return (
     <div className="flex w-full flex-col gap-x-10 gap-y-6 text-neutrals-800 odd:md:flex-row even:md:flex-row-reverse lg:items-center lg:gap-x-16 2xl:gap-x-40">
@@ -26,14 +25,9 @@ const InfoSection = ({
         alt="card"
         width={4000}
         height={2667}
-        className={cn(
-          "h-[22.5rem] w-full min-w-64 rounded-2xl object-cover md:w-3/4 lg:h-[43.75rem] lg:w-2/3 lg:rounded-20 2xl:w-1/2",
-          position
-            ? `${position} lg:${position}`
-            : "object-[30%] lg:object-[60%]",
-          smPosition && "sm:object-[center_10%]",
-        )}
+        className="h-[22.5rem] w-full min-w-64 rounded-2xl object-cover md:w-3/4 lg:h-[43.75rem] lg:w-2/3 lg:rounded-20 2xl:w-1/2"
         priority
+        style={{ objectPosition: `${xPosition}% ${yPosition}%` }}
       />
       <div className="flex flex-col justify-center gap-y-6 lg:gap-y-10 xl:max-w-lg">
         <Typography variant="h2" tag="h2" fontWeight="bold">
