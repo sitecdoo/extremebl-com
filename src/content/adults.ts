260839b5952aab5a59c59629e336ddd2289d0477
Andrej <andrejjurisic99@gmail.com>
2024-12-27 14:36:27 +0100

    update content objects

--- src/content/adults.ts
+++ src/content/adults.ts
@@ -18,14 +18,64 @@ export const pricingData = {
 };
 
 export const carouselImages = [
-  { src: "/adults/adults-carousel-1.jpg", alt: "carousel image" },
-  { src: "/adults/adults-carousel-2.jpg", alt: "carousel image" },
-  { src: "/adults/adults-carousel-3.jpg", alt: "carousel image" },
-  { src: "/adults/adults-carousel-4.jpg", alt: "carousel image" },
-  { src: "/adults/adults-carousel-5.jpg", alt: "carousel image" },
-  { src: "/adults/adults-carousel-6.jpg", alt: "carousel image" },
-  { src: "/adults/adults-carousel-7.jpg", alt: "carousel image" },
-  { src: "/adults/adults-carousel-8.jpg", alt: "carousel image" },
-  { src: "/adults/adults-carousel-9.jpg", alt: "carousel image" },
-  { src: "/adults/adults-carousel-10.jpg", alt: "carousel image" },
+  {
+    src: "/adults/adults-carousel-1.jpg",
+    alt: "carousel image",
+    xPosition: 60,
+    yPosition: 50,
+  },
+  {
+    src: "/adults/adults-carousel-2.jpg",
+    alt: "carousel image",
+    xPosition: 50,
+    yPosition: 35,
+  },
+  {
+    src: "/adults/adults-carousel-3.jpg",
+    alt: "carousel image",
+    xPosition: 50,
+    yPosition: 65,
+  },
+  {
+    src: "/adults/adults-carousel-4.jpg",
+    alt: "carousel image",
+    xPosition: 50,
+    yPosition: 60,
+  },
+  {
+    src: "/adults/adults-carousel-5.jpg",
+    alt: "carousel image",
+    xPosition: 100,
+    yPosition: 50,
+  },
+  {
+    src: "/adults/adults-carousel-6.jpg",
+    alt: "carousel image",
+    xPosition: 99,
+    yPosition: 1,
+  },
+  {
+    src: "/adults/adults-carousel-7.jpg",
+    alt: "carousel image",
+    xPosition: 90,
+    yPosition: 50,
+  },
+  {
+    src: "/adults/adults-carousel-8.jpg",
+    alt: "carousel image",
+    xPosition: 60,
+    yPosition: 60,
+  },
+  {
+    src: "/adults/adults-carousel-9.jpg",
+    alt: "carousel image",
+    xPosition: 30,
+    yPosition: 60,
+  },
+  {
+    src: "/adults/adults-carousel-10.jpg",
+    alt: "carousel image",
+    xPosition: 50,
+    yPosition: 50,
+  },
 ];
--- src/content/children.ts
+++ src/content/children.ts
@@ -45,14 +45,64 @@ export const infoSectionData = [
 ];
 
 export const carouselImages = [
-  { src: "/children/carousel-1.jpg", alt: "Children stretching at the gym" },
-  { src: "/children/carousel-2.jpg", alt: "Child climbing the gym wall" },
-  { src: "/children/carousel-3.jpg", alt: "Child climbing the gym wall" },
-  { src: "/children/carousel-4.jpg", alt: "Child climbing the gym wall" },
-  { src: "/children/carousel-5.jpg", alt: "Children group photo at the gym" },
-  { src: "/children/carousel-6.jpg", alt: "Children climbing the gym wall" },
-  { src: "/children/carousel-7.jpg", alt: "Child climbing the gym wall" },
-  { src: "/children/carousel-8.jpg", alt: "Children stretching at the gym" },
-  { src: "/children/carousel-9.jpg", alt: "Children stretching at the gym" },
-  { src: "/children/carousel-10.jpg", alt: "Child climbing the wall" },
+  {
+    src: "/children/carousel-1.jpg",
+    alt: "Children stretching at the gym",
+    xPosition: 65,
+    yPosition: 50,
+  },
+  {
+    src: "/children/carousel-2.jpg",
+    alt: "Child climbing the gym wall",
+    xPosition: 100,
+    yPosition: 50,
+  },
+  {
+    src: "/children/carousel-3.jpg",
+    alt: "Child climbing the gym wall",
+    xPosition: 50,
+    yPosition: 40,
+  },
+  {
+    src: "/children/carousel-4.jpg",
+    alt: "Child climbing the gym wall",
+    xPosition: 50,
+    yPosition: 50,
+  },
+  {
+    src: "/children/carousel-5.jpg",
+    alt: "Children group photo at the gym",
+    xPosition: 40,
+    yPosition: 50,
+  },
+  {
+    src: "/children/carousel-6.jpg",
+    alt: "Children climbing the gym wall",
+    xPosition: 50,
+    yPosition: 50,
+  },
+  {
+    src: "/children/carousel-7.jpg",
+    alt: "Child climbing the gym wall",
+    xPosition: 50,
+    yPosition: 50,
+  },
+  {
+    src: "/children/carousel-8.jpg",
+    alt: "Children stretching at the gym",
+    xPosition: 50,
+    yPosition: 50,
+  },
+  {
+    src: "/children/carousel-9.jpg",
+    alt: "Children stretching at the gym",
+    xPosition: 50,
+    yPosition: 50,
+  },
+  {
+    src: "/children/carousel-10.jpg",
+    alt: "Child climbing the wall",
+    xPosition: 50,
+    yPosition: 40,
+  },
 ];
