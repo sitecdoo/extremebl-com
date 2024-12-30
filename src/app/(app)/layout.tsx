f1370d5ce81c3aebc59f43e8ae3f77050bc87d29
Andrej <andrejjurisic99@gmail.com>
2024-12-30 14:25:18 +0100

    implement toploader

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import Footer from "@/components/custom-ui/footer";
 import Navbar from "@/components/custom-ui/navbar/navbar";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
+import NextTopLoader from "nextjs-toploader";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -21,6 +22,7 @@ export default function RootLayout({
         className={`${leagueSpartan.variable} ${openSans.variable} bg-neutrals-50 font-league-spartan antialiased`}
       >
         <div className="relative overflow-x-clip">
+          <NextTopLoader color="#517B83" height={4} />
           <Navbar />
           <div className="mx-auto flex w-full max-w-[108rem] flex-col items-center overflow-clip px-4 lg:px-12 3xl:overflow-x-visible">
             <NuqsAdapter>{children}</NuqsAdapter>
