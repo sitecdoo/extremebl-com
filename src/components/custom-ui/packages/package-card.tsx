0877720435dd61be9ecd38dbc332b79b3337d24c
Andrej <andrejjurisic99@gmail.com>
2025-02-11 13:42:59 +0100

    update packages component

--- src/components/custom-ui/packages/package-card.tsx
+++ src/components/custom-ui/packages/package-card.tsx
@@ -1,16 +1,23 @@
 import { Check } from "lucide-react";
 import Typography from "../typography";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 type PackageCardProps = {
   name: string;
   price: string;
   offers: string[];
+  className?: string;
 };
 
-const PackageCard = ({ name, price, offers }: PackageCardProps) => {
+const PackageCard = ({ name, price, offers, className }: PackageCardProps) => {
   return (
-    <div className="flex w-80 min-w-60 max-w-80 flex-col gap-y-8 rounded-20 bg-neutrals-100 px-12 py-8 shadow-sm lg:w-[22.5rem] lg:max-w-[22.5rem] lg:gap-y-12 lg:px-14 lg:py-12">
+    <div
+      className={cn(
+        "flex w-80 min-w-60 max-w-80 flex-col gap-y-8 rounded-20 bg-neutrals-100 px-12 py-8 shadow-sm lg:w-[22.5rem] lg:max-w-[22.5rem] lg:gap-y-12 lg:px-14 lg:py-12",
+        className,
+      )}
+    >
       <div className="flex flex-col gap-y-6 lg:gap-y-9">
         <div className="flex w-fit flex-col items-center gap-3 self-center">
           <Typography variant="body-sm" uppercase>
--- src/components/custom-ui/packages/packages-section.tsx
+++ src/components/custom-ui/packages/packages-section.tsx
@@ -13,7 +13,7 @@ const PackagesSection = () => {
       <Typography tag="h2" variant="h2" fontWeight="bold">
         Paketi
       </Typography>
-      <div className="flex w-full justify-center rounded-20 px-10 md:bg-[#F7F7F7]">
+      <div className="flex w-full justify-center">
         <div className="grid grid-cols-1 justify-items-center gap-5 md:grid-cols-2 lg:gap-8 xl:grid-cols-3">
           {data.map((item, index) => (
             <PackageCard
@@ -21,6 +21,7 @@ const PackagesSection = () => {
               name={item.name}
               price={item.price}
               offers={item.offers}
+              className={`${index === 2 && "md:col-span-2 md:justify-self-center xl:col-span-1"}`}
             />
           ))}
         </div>
