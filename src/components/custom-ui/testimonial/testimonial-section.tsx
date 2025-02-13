fbe083769329508866a72b223681b2437739035b
Andrej <andrejjurisic99@gmail.com>
2025-02-13 13:21:36 +0100

    connect other pages

--- src/components/custom-ui/testimonial/testimonial-section.tsx
+++ src/components/custom-ui/testimonial/testimonial-section.tsx
@@ -1,8 +1,11 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Testimonial from "./testimonial";
 import Typography from "../typography";
 import { PropsWithChildren } from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 type TestemonialSectionProps = PropsWithChildren<{
   description: string;
@@ -16,12 +19,15 @@ const TestimonialSection = ({
   image,
   children,
 }: TestemonialSectionProps) => {
+  const pathname = usePathname();
+  const routePathanme = pathname.split("/")[2];
+
   return (
     <div className="flex w-full flex-col items-center gap-6 sm:px-10 md:px-16 lg:gap-12 lg:px-20 xl:px-36">
       <Testimonial image={image} name={name} description={description}>
         {children}
       </Testimonial>
-      <Link href="/kontakt">
+      <Link href={`kontakt?subject=${encodeURIComponent(routePathanme)}`}>
         <Button variant="blue" className="w-fit">
           <Typography fontWeight="bold">Kontaktirajte nas</Typography>
         </Button>
