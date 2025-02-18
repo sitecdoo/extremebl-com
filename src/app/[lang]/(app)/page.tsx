aae37c48e66ab5c3764b23409417f17854151d72
Andrej <andrejjurisic99@gmail.com>
2025-02-18 11:22:03 +0100

    ...

--- src/app/[lang]/(app)/page.tsx
+++ src/app/[lang]/(app)/page.tsx
@@ -24,8 +24,6 @@ export default async function HomePage() {
   const dict = await getDictionary();
   const { docs } = await getRecentPosts();
 
-  console.log(docs);
-
   return (
     <div className="relative flex w-full flex-col items-center gap-24 pb-24 lg:gap-64 lg:pb-64">
       <HomeBannerBlobs />
